2c088d3d3f975ff8cbf717d14b978e961c1112ae
kareenaarora <kkarora2004@gmail.com>
2025-04-22 14:30:28 -0400

    zip done

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def main():
-    pass
+from zipfile import ZipFile
+
+
+
+
+
+def zip():
+    with ZipFile('data.zip', 'w') as zip1:
+
+        zip1.write("/Users/kareenaarora/Desktop/beg pyt/script1")
 
 if __name__ == '__main__':
-    pass
+    zip()
