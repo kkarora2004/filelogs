2506302239a6748e0414195156c112aa8ddcb1b8
kareenaarora <kkarora2004@gmail.com>
2025-04-29 14:22:48 -0400

    api done

--- main.py
+++ main.py
@@ -1,13 +1,72 @@
-from zipfile import ZipFile
+import json
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+foods = [
+ { 'id': 1, 'name': 'BBQ' },
+ { 'id': 2, 'name': 'chicken' },
+ { 'id': 3, 'name': 'banana' }
+]
 
 
+nextFoodId = 4
+
+@app.route('/foods', methods=['GET'])
+def get_foods():
+ return jsonify(foods)
+
+@app.route('/foods/<int:id>', methods=['GET'])
+def get_food_by_id(id: int):
+ food = get_food(id)
+ if food is None:
+   return jsonify({ 'error': 'food does not exist'}), 404
+ return jsonify(food)
+
+def get_food(id):
+ return next((f for f in foods if f['id'] == id), None)
+
+def food_is_valid(food):
+ for key in food.keys():
+   if key != 'name':
+    return False
+ return True
+
+@app.route('/foods', methods=['POST'])
+def create_food():
+    global nextFoodId
+    food = request.get_json()  # <-- fix here
+    if not food_is_valid(food):
+        return jsonify({ 'error': 'Invalid food properties.' }), 400
+
+    food['id'] = nextFoodId
+    nextFoodId += 1
+    foods.append(food)
+
+    return '', 201, { 'location': f'/foods/{food["id"]}' }
+
+@app.route('/foods/<int:id>', methods=['PUT'])
+def update_food(id: int):
+    food = get_food(id)
+    if food is None:
+        return jsonify({ 'error': 'food does not exist.' }), 404
+
+    updated_food = request.get_json()  # <-- fix here
+    if not food_is_valid(updated_food):
+        return jsonify({ 'error': 'Invalid food properties.' }), 400
+
+    food.update(updated_food)
+    return jsonify(food)
 
 
+@app.route('/foods/<int:id>', methods=['DELETE'])
+def delete_food(id: int):
+ global foods
+ food = get_food(id)
+ if food is None:
+   return jsonify({ 'error': 'food does not exist.' }), 404
 
-def zip():
-    with ZipFile('data.zip', 'w') as zip1:
-
-        zip1.write("/Users/kareenaarora/Desktop/beg pyt/script1")
+ foods = [f for f in foods if f['id'] != id]
+ return jsonify(foods), 200
 
 if __name__ == '__main__':
-    zip()
+   app.run(port=5000)
