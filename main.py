c050352d46073500f22a0d29d390f39f7b08d2c3
kareenaarora <kkarora2004@gmail.com>
2025-05-08 15:58:43 -0400

    google api console upload file

--- main.py
+++ main.py
@@ -1,72 +1,90 @@
-import json
-from flask import Flask, jsonify, request
-app = Flask(__name__)
-
-foods = [
- { 'id': 1, 'name': 'BBQ' },
- { 'id': 2, 'name': 'chicken' },
- { 'id': 3, 'name': 'banana' }
-]
-
-
-nextFoodId = 4
-
-@app.route('/foods', methods=['GET'])
-def get_foods():
- return jsonify(foods)
-
-@app.route('/foods/<int:id>', methods=['GET'])
-def get_food_by_id(id: int):
- food = get_food(id)
- if food is None:
-   return jsonify({ 'error': 'food does not exist'}), 404
- return jsonify(food)
-
-def get_food(id):
- return next((f for f in foods if f['id'] == id), None)
-
-def food_is_valid(food):
- for key in food.keys():
-   if key != 'name':
-    return False
- return True
-
-@app.route('/foods', methods=['POST'])
-def create_food():
-    global nextFoodId
-    food = request.get_json()  # <-- fix here
-    if not food_is_valid(food):
-        return jsonify({ 'error': 'Invalid food properties.' }), 400
-
-    food['id'] = nextFoodId
-    nextFoodId += 1
-    foods.append(food)
-
-    return '', 201, { 'location': f'/foods/{food["id"]}' }
-
-@app.route('/foods/<int:id>', methods=['PUT'])
-def update_food(id: int):
-    food = get_food(id)
-    if food is None:
-        return jsonify({ 'error': 'food does not exist.' }), 404
-
-    updated_food = request.get_json()  # <-- fix here
-    if not food_is_valid(updated_food):
-        return jsonify({ 'error': 'Invalid food properties.' }), 400
-
-    food.update(updated_food)
-    return jsonify(food)
-
-
-@app.route('/foods/<int:id>', methods=['DELETE'])
-def delete_food(id: int):
- global foods
- food = get_food(id)
- if food is None:
-   return jsonify({ 'error': 'food does not exist.' }), 404
-
- foods = [f for f in foods if f['id'] != id]
- return jsonify(foods), 200
-
-if __name__ == '__main__':
-   app.run(port=5000)
+# import json
+# from flask import Flask, jsonify, request
+# app = Flask(__name__)
+#
+# foods = [
+#  { 'id': 1, 'name': 'BBQ' },
+#  { 'id': 2, 'name': 'chicken' },
+#  { 'id': 3, 'name': 'banana' }
+# ]
+#
+#
+# nextFoodId = 4
+#
+# @app.route('/foods', methods=['GET'])
+# def get_foods():
+#  return jsonify(foods)
+#
+# @app.route('/foods/<int:id>', methods=['GET'])
+# def get_food_by_id(id: int):
+#  food = get_food(id)
+#  if food is None:
+#    return jsonify({ 'error': 'food does not exist'}), 404
+#  return jsonify(food)
+#
+# def get_food(id):
+#  return next((f for f in foods if f['id'] == id), None)
+#
+# def food_is_valid(food):
+#  for key in food.keys():
+#    if key != 'name':
+#     return False
+#  return True
+#
+# @app.route('/foods', methods=['POST'])
+# def create_food():
+#     global nextFoodId
+#     food = request.get_json()  # <-- fix here
+#     if not food_is_valid(food):
+#         return jsonify({ 'error': 'Invalid food properties.' }), 400
+#
+#     food['id'] = nextFoodId
+#     nextFoodId += 1
+#     foods.append(food)
+#
+#     return '', 201, { 'location': f'/foods/{food["id"]}' }
+#
+# @app.route('/foods/<int:id>', methods=['PUT'])
+# def update_food(id: int):
+#     food = get_food(id)
+#     if food is None:
+#         return jsonify({ 'error': 'food does not exist.' }), 404
+#
+#     updated_food = request.get_json()  # <-- fix here
+#     if not food_is_valid(updated_food):
+#         return jsonify({ 'error': 'Invalid food properties.' }), 400
+#
+#     food.update(updated_food)
+#     return jsonify(food)
+#
+#
+# @app.route('/foods/<int:id>', methods=['DELETE'])
+# def delete_food(id: int):
+#  global foods
+#  food = get_food(id)
+#  if food is None:
+#    return jsonify({ 'error': 'food does not exist.' }), 404
+#
+# #  foods = [f for f in foods if f['id'] != id]
+# #  return jsonify(foods), 200
+# #
+# # if __name__ == '__main__':
+# #    app.run(port=5000)
+#
+#
+# # import httplib
+# from pydrive.drive import GoogleDrive
+# from pydrive.auth import GoogleAuth
+#
+# import os
+#
+# guath = GoogleAuth()
+# guath.LocalWebserverAuth()
+# drive = GoogleDrive(guath)
+#
+# path = "/Users/kareenaarora/Desktop/testing123"
+#
+# for x in os.listdir(path):
+#     f = drive.CreateFile({'title': x})
+#     f.SetContentFile(os.path.join(path, x))
+#     f.Upload()
